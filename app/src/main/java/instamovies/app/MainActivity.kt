dade351994c7732860583e524b427d17a8afdd51
Mubashir P A <mubashirpa2002@gmail.com>
2023-11-22 15:24:20 +0530

    Add enableEdgeToEdge

--- app/src/main/java/instamovies/app/MainActivity.kt
+++ app/src/main/java/instamovies/app/MainActivity.kt
@@ -3,6 +3,7 @@ package instamovies.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -20,6 +21,7 @@ class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
         setContent {
