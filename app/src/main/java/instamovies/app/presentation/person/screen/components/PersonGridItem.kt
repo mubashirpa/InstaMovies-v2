66dd69df67a31cd63d06784a707ec2001b53120a
Mubashir P A <mubashirpa2002@gmail.com>
2023-12-03 18:23:42 +0530

    Removed ExperimentalMaterial3Api OptIn

--- app/src/main/java/instamovies/app/presentation/person/screen/components/PersonGridItem.kt
+++ app/src/main/java/instamovies/app/presentation/person/screen/components/PersonGridItem.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
