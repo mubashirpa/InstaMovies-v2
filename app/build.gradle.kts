60f27e694ad795ea104aab921527f6d34d410968
Mubashir P A <mubashirpa2002@gmail.com>
2025-04-26 13:01:15 +0530

    Refactor: Update compileSdk and targetSdk to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,12 +20,12 @@ plugins {
 
 android {
     namespace = "instamovies.app"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "instamovies.app"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
