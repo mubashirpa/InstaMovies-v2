3a1138b7068bfeb524ebe544d78e97ba0952953c
Mubashir P A <mubashirpa2002@gmail.com>
2025-05-08 17:28:59 +0530

    Refactor: Update `enumValueOf` and remove unused `LoadingIndicator`

--- app/src/main/java/instamovies/app/presentation/components/LoadingIndicator.kt
+++ app/src/main/java/instamovies/app/presentation/components/LoadingIndicator.kt
@@ -7,21 +7,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
-fun LoadingIndicator(
-    progress: Float,
-    modifier: Modifier = Modifier,
-) {
-    Box(
-        modifier = modifier,
-        contentAlignment = Alignment.Center,
-    ) {
-        CircularProgressIndicator(
-            progress = { progress },
-        )
-    }
-}
-
-@Composable
 fun LoadingIndicator(modifier: Modifier = Modifier) {
     Box(
         modifier = modifier,
