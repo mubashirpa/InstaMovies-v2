e33c1f5d93bf877bc6adc35378d92a4aea8b4577
Mubashir P A <mubashirpa2002@gmail.com>
2025-05-08 17:40:54 +0530

    Refactor: Update error screen UI

--- app/src/main/java/instamovies/app/presentation/components/ErrorScreen.kt
+++ app/src/main/java/instamovies/app/presentation/components/ErrorScreen.kt
@@ -1,7 +1,6 @@
 package instamovies.app.presentation.components
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,32 +9,16 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import instamovies.app.presentation.theme.InstaMoviesTheme
 import instamovies.app.R.string as Strings
-
-@Composable
-fun ErrorScreen(
-    modifier: Modifier = Modifier,
-    message: String = stringResource(id = Strings.error_unexpected),
-) {
-    Box(
-        modifier = modifier,
-        contentAlignment = Alignment.Center,
-    ) {
-        Text(
-            text = message,
-            textAlign = TextAlign.Center,
-        )
-    }
-}
 
 @Composable
 fun ErrorScreen(
@@ -88,18 +71,17 @@ enum class Axis {
     Vertical,
 }
 
+@Preview(showBackground = true)
 @Composable
 private fun ErrorScreenPreview() {
     InstaMoviesTheme {
-        Surface {
-            ErrorScreen(
-                onRetry = {},
-                modifier =
-                    Modifier
-                        .fillMaxSize()
-                        .padding(16.dp),
-                direction = Axis.Horizontal,
-            )
-        }
+        ErrorScreen(
+            onRetry = {},
+            modifier =
+                Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+            direction = Axis.Horizontal,
+        )
     }
 }
